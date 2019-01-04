86999f2537179498401b4532ffc52d379396b46b
Charlotte Koch <dressupgeekout@gmail.com>
2019-01-04 00:35:34 -0800

    Rite object validations; some styling

--- models/rite.rb
+++ models/rite.rb
@@ -4,6 +4,18 @@ class Rite < Sequel::Model
   plugin :validation_helpers
 
   def validate
-    self
+    super
+
+    if self.player_a_id == self.player_b_id
+      errors.add(:player_a_id, "Player A cannot be the same as Player B")
+      errors.add(:player_b_id, "Player B cannot be the same as Player A")
+    end
+
+    validates_operator :>, 0, :duration
+    validates_operator :<=, 4, :masteries_allowed
+
+    if self.player_a_pyre_end_health <= 0 && self.player_b_pyre_end_health <= 0
+      errors.add("Both players lost the rite?!?")
+    end
   end
 end
